b12e5a64d1eda53ffde867a139444fa4c73c1786
seaniiio <121426422+seaniiio@users.noreply.github.com>
2023-10-12 12:27:41 +0900

    Update lv2_기능개발.py

--- seaniiio/PGS/스택/큐/lv2_기능개발.py
+++ seaniiio/PGS/스택/큐/lv2_기능개발.py
@@ -1,3 +1,10 @@
+'''
+- 개발해야 하는 여러 기능들이 존재
+- 진행 상황과 진행 속도가 각기 다름
+- 뒤에있는 기능이 앞의 기능보다 먼저 개발되더라도 앞의 기능이 완성되어야 배포 가능
+- 따라서 “현재 가장 우선순위가 높은 기능의 현황”만 추적하면 됨
+'''
+
 from collections import deque
 def solution(progresses, speeds):
     q = deque(progresses)
@@ -19,6 +26,7 @@ def solution(progresses, speeds):
 
 
 # 인덱스 이용
+# 작업속도, 속도 두 개의 큐를 건들(pop해줄) 필요 없이, 그냥 인덱스 하나만 이동하면 됨.
 def solution(p, s):
     answer = []
     top_index = 0 # 현재(top) 인덱스
