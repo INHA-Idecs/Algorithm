767742bc438edd9559cad2f90ed420a952e60569
seaniiio <121426422+seaniiio@users.noreply.github.com>
2023-10-12 12:14:05 +0900

    Update lv3_베스트앨범.py

--- seaniiio/PGS/해시/lv3_베스트앨범.py
+++ seaniiio/PGS/해시/lv3_베스트앨범.py
@@ -1,21 +1,26 @@
+'''
+- 장르별로 가장 많이 재생된 두 곡씩 뽑아서 “베스트 앨범”을 만들어야 함
+- 이 때, 수록할 곡의 순서를 고려해야 함
+1. 속한 노래들의 재생횟수 합이 가장 큰 장르를 먼저
+2. 같은 장르 내에서는 재생횟수가 큰 노래를 먼저, 같다면 고유번호(입력된 순서)가 작은 노래를 먼저
+'''
+
 def solution(genres, plays):
     answer = []
+    # 장르를 key로, 그 장르에 속하는 노래들의 정보를 value로 하는 딕셔너리.
+    # key:value => 'pop' : [500(장르의 총 재생수), [500, 0(고유번호)]]
     albums = {}
     for i in range(len(genres)):
         if genres[i] not in albums:
             albums[genres[i]] = [plays[i]]
-            
-            albums[genres[i]].append([[plays[i], i]]) # 형태 => 'pop' : [1100(장르의 총 재생수), [500, 1(고유번호)]]
+            albums[genres[i]].append([[plays[i], i]]) 
         else:
             albums[genres[i]][0] = albums[genres[i]][0] + plays[i]
             albums[genres[i]].append([[plays[i], i]])
-    print(list(albums.values()))
+            
     albums = sorted(albums.values(), key=lambda x : x[0], reverse=True) # 노래 재생 기준으로 장르 정렬
-    print(albums)
-    
     for i in range(len(albums)): # 장르 개수만큼
         song_list = albums[i][1:]
-        print(song_list)
         song_list_ordered = sorted(song_list, key=lambda x : x[0][0], reverse=True)
         answer.append(song_list_ordered[0][0][1])
         if len(song_list) > 1 : answer.append(song_list_ordered[1][0][1])
